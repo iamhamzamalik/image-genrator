5ab0c1ffa44aff9732973eed61db897759fce35f
Hamza malik <ami.zeemalik@gmail.com>
2023-01-03 15:55:39 +0500

    commit

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { generateimage } = require('../controllers/openaiController');
+
+
+const router = express.Router();
+
+router.post('/generateimage', generateimage);
+ 
+
+module.exports = router;
